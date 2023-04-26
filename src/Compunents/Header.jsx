8e33e4d686c9304f17e854e5573cb46c9053740e
RaviAtal <raviatal44@gmail.com>
2023-04-26 10:05:48 +0530

    first commit

---
+++ src/Compunents/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <section className=" bg-danger py-5">
+        <div className="container">
+          <h1>Lorem ipsum dolor sit amet.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsam
+            architecto sed ducimus error magnam tenetur est, dolore nisi
+            delectus fuga asperiores deserunt perspiciatis vel? Nam dolores
+            architecto iure vero.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Header;
